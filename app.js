162feb1889e8215eb5d1282287b58857b8189c4a
mironenkovasila <mironenkovasila@gmail.com>
2022-03-01 15:57:20 +0200

    Refactor blog

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const bodyParser = require("body-parser");
 const session = require("express-session");
+const expressValidator = require("express-validator");
 // const multer = require("multer");
 // const upload = multer({ dest: "uploads/" });
-const expressValidator = require("express-validator");
+const createError = require("http-errors");
+const path = require("path");
+const bodyParser = require("body-parser");
 const mongooes = require("mongoose");
+const connectFlash = require("connect-flash");
 
+console.log(process.env.DB_URL);
 mongooes
   .connect(process.env.DB_URL)
   .then(() => console.log("Connected..."))
@@ -18,8 +20,6 @@ mongooes
 const indexRouter = require("./routes/index");
 const postsRouter = require("./routes/posts");
 const categoriesRouter = require("./routes/categories");
-const authorsRouter = require("./routes/authors");
-const registerRouter = require("./routes/register");
 
 const app = express();
 
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("connect-flash")());
+app.use(connectFlash());
 app.use((req, res, next) => {
   res.locals.messages = require("express-messages")(req, res);
   next();
@@ -82,8 +82,6 @@ app.use((req, res, next) => {
 app.use("/", indexRouter);
 app.use("/posts", postsRouter);
 app.use("/categories", categoriesRouter);
-app.use("/authors", authorsRouter);
-app.use("/register", registerRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
